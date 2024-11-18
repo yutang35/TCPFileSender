53a651aaeaa967a81756dc3d494c9fe45d67a06d
yclin <lin8yc@gmail.com>
2024-11-18 13:06:13 +0800

    Initial commit in local repository

---
+++ TcpFileSender.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-11-12T10:30:42
+#
+#-------------------------------------------------
+
+QT       += core gui network
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = TcpFileSender
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        tcpfilesender.cpp
+
+HEADERS  += tcpfilesender.h
---
+++ tcpfilesender.h
@@ -0,0 +1,38 @@
+#ifndef TCPFILESENDER_H
+#define TCPFILESENDER_H
+
+#include <QDialog>
+#include <QtNetwork>
+#include <QtWidgets>
+
+class TcpFileSender : public QDialog
+{
+    Q_OBJECT
+
+public:
+    TcpFileSender(QWidget *parent = 0);
+    ~TcpFileSender();
+public slots:
+    void start();
+    void startTransfer();
+    void updateClientProgress(qint64 numBytes);
+    void openFile();
+private:
+    QProgressBar     *clientProgressBar;
+    QLabel           *clientStatusLabel;
+    QPushButton      *startButton;
+    QPushButton      *quitButton;
+    QPushButton      *openButton;
+    QDialogButtonBox *buttonBox;
+    QTcpSocket       tcpClient;
+
+    qint64           totalBytes;
+    qint64           bytesWritten;
+    qint64           bytesToWrite;
+    qint64           loadSize;
+    QString          fileName;
+    QFile            *localFile;
+    QByteArray       outBlock;
+};
+
+#endif // TCPFILESENDER_H
